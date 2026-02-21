77b635dd1c69b1569c291bd922e80e01a77aa6ab
Jon <coryellj1@students.westerntc.edu>
2026-02-20 19:41:59 -0600

    step 4 done

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
+
 app.get('/', (req, res) => {
   res.render('home', {
      title: "My Home Page"
@@ -55,6 +56,18 @@ app.get('/contact', (req, res) => {
 
 app.post('/contact/submit', (req, res) => {
   res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
+});
+
+app.get("/404", (req, res) => {
+  res.status(404);
+  res.render('default-layout', {
+     title: "Page Not Found",
+     content: "<h1>Sorry!</h1><h3>We can't find the page you're requesting.</h3>"
+  });
+});
+
+app.all('/{*splat}', (req, res) => {
+  res.status(404).redirect("/404");
 });
 
 // START THE SERVER
